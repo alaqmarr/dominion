54515621a2d33df7bcba65bb2eff6763ca0d5711
Al Aqmar <alaqmarak0810@gmail.com>
2025-07-15 15:08:14 +0530

    test

---
+++ src/app/(site)/products/page.tsx
@@ -0,0 +1,39 @@
+import SingleBlog from "@/components/Blog/SingleBlog";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import { getAllPosts } from "@/utils/markdown";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "Blog Grids | Play SaaS Starter Kit and Boilerplate for Next.js",
+  description: "Blog grids page description",
+};
+
+const Products = async () => {
+  const posts = await fetch(`https://mystore.alaqmar.dev/api/dominion/products`,
+    {
+      next: { revalidate: 60 }, // Revalidate every 60 seconds
+    }
+  )
+    .then((res) => res.json())
+
+  return (
+    <>
+      <Breadcrumb pageName="Products" />
+
+      <section className="pb-10 pt-20 lg:pb-20 lg:pt-[120px]">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap justify-center">
+            {posts.map((product: any, i: any) => (
+              <div key={i} className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3">
+                <SingleBlog blog={product} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Products;
